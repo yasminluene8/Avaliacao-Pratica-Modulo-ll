5c3b744017af489e27b0b192fe752109fbdf539b
yasminluene8 <yasminluene8@gmail.com>
2025-10-25 09:09:14 -0300

    Add basic arithmetic routes to Flask app

--- app_flask.py
+++ app_flask.py
@@ -8,6 +8,32 @@ def index():
     return render_template("index.html")
 
 ## Continue o código aqui.
+@app.route('/soma')
+def soma():
+    v1=float(request.args.get('valor1',0))
+    v2=float(request.args.get('valor2',0))
+    return{'resultado':v1 +v2}
+
+@app.route('/subtrair')
+def subtrair():
+    v1=float(request.args.get('valor1',0))
+    v2=float(request.args.get('valor2',0))
+    return{'resultado':v1 -v2}
+
+@app.route('/mutiplicar')
+def mutiplicar():
+    v1=float(request.args.get('valor1',0))
+    v2=float(request.args.get('valor2',0))
+    return{'resultado':v1 * v2}
+
+@app.route('/dividir')
+def dividir():
+    v1=float(request.args.get('valor1',0))
+    v2=float(request.args.get('valor2',0))
+    if v2 == 0:
+        return{'erro':'Operação não permitida.'}
+    return{'resultado': v1/v2}
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
